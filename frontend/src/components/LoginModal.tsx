99970cbfd9fb686534f8898fef15c11630aec792
Christopher Wangsanata <chriswangsanata@gmail.com>
2024-10-11 16:18:42 -0700

    update modal with React95 components

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,37 +1,25 @@
 import React from 'react';
 import { Modal, ModalMenu } from '@react95/core/Modal';
+import { Button, TextInput } from 'react95';
+
+import '../styles/loginModal.css';
 
 const LoginModal: React.FC< ModalMenu > = () => {
-  return (
-    <Modal
-      title='Login'
-      dragOptions={{
-        defaultPosition: {
-          x: 130,
-          y: 130
-        }
-      }}
-    >
-      <form>
-        <label>
-          Username:
-          <input type='text' />
-        </label>
-        <label>
-          Password:
-          <input type='password' />
-        </label>
-      </form>
+  return <>
+    <Modal title="Login" className="login-modal" dragOptions={{
+      defaultPosition: {
+        x: 0,
+        y: 20
+      }
+    }}>
       <Modal.Content width="300px" height="160px" boxShadow="$in" bgColor="white">
-            <p>
-              The active modal will be based on the order they render, most
-              recently rendered will be the active component. On click of a
-              non-active modal will fire an action to set that modal as the
-              active one.
-            </p>
+        <TextInput placeholder="Username" />
+        <TextInput placeholder="Password" type="password" />
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
       </Modal.Content>
     </Modal>
-  );
-}
+  </>;
+};
 
 export default LoginModal;
